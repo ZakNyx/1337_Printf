8a63c6b4618c5550457d16307fc43db2998d5796
ZakNyx <gobuwu@hotmail.com>
2021-11-20 02:53:18 +0100

    header/makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,34 @@
+NAME = libftprintf.a
+
+AR = ar -rc
+
+GCC = gcc -c
+
+RM = rm -rf
+
+CFLAGS = -Wall -Werror -Wextra -I $(LIBFT_DIR) 
+
+LIBFT_DIR = libft
+
+LIBFT_PATH = ${LIBFT_DIR}/libft.a/
+
+LIBFT_MAKE = Makefile
+
+FILES = ft_parse.c \
+	ft_printf.c \
+	ft_printf_address.c \
+	ft_printf_chars.c \
+	ft_printf_numbers.c \
+	ft_printf_tools 
+
+OBJS = $(subst .c,.o,$(FILES))
+
+all : $(NAME)
+
+$(NAME) : $(OBJS) $(LIBFT_DIR)
+	$(CC) $(CFLAGS) $(FILES)
+	$(AR) $(NAME) $(OBJS)
+
 git:
 	git add .
 	git commit -m "$m"
--- printf.h
+++ printf.h
@@ -13,10 +13,28 @@
 #ifndef PRINTF_H
 # define PRINTF_H
 
+# include <stdint.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
 # include <stdarg.h>
 # include <stdio.h>
 # include "../libft/libft.h"
+
+int	ft_printf(const char *format, ...);
+int	ft_counter(int num);
+int	ft_counter_unsint(unsigned int num);
+int	ft_putnbr_hxd(long int num, chqr *base, int fd);
+int	ft_putnbr_hxd_ad(uintptr_t num, char *base, int fd);
+int	ft_putnbr_unsint(unsigned int num, int fd);
+int	ft_print_character(va_list ap);
+int	ft_print_pourcentage(void);
+int	ft_print_string(va_list ap);
+int	ft_parse(char flag, va_list ap);
+int	flag_x(va_list ap);
+int	flag_X(va_list ap);
+int	flag_u(va_list ap);
+int	flag_i(va_list ap);
+int	flag_d(va_list ap);
+int	flag_p(va_list ap);
 #endif
